caa767a477874e6554d5f5afeee80ae2d8be366a
lukuta <kutalukasz@gmail.com>
2017-09-03 20:06:40 +0200

    added BettingTimeRestriction entity class

---
+++ src/main/java/com/goldentwo/model/BettingTimeRestriction.java
@@ -0,0 +1,25 @@
+package com.goldentwo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BettingTimeRestriction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private LocalTime timeFrom;
+    private LocalTime timeTo;
+}
