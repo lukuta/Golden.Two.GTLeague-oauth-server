5dcd57ea4c1b45565d6bb2e5e5a04cba0fbac6b4
lukuta <kutalukasz@gmail.com>
2017-09-03 22:47:14 +0200

    added InMemoryUserDetailsManager with two custom users

--- src/main/java/com/goldentwo/config/OAuth2ServerConfig.java
+++ src/main/java/com/goldentwo/config/OAuth2ServerConfig.java
@@ -2,22 +2,42 @@ package com.goldentwo.config;
 
 import com.goldentwo.security.CustomAuthenticationEntryPoint;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import java.util.Properties;
+
 @Configuration
-@EnableOAuth2Client
 @EnableAuthorizationServer
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class OAuth2ServerConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String ADMIN_ROLE = "ADMIN";
+    private static final String USER_ROLE = "USER";
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(inMemoryUserDetailsManager());
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        final Properties users = new Properties();
+        users.put("user", "password,ROLE_USER,enabled");
+        users.put("admin", "password,ROLE_USER, ROLE_ADMIN ,enabled");
+        return new InMemoryUserDetailsManager(users);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -38,6 +58,9 @@ public class OAuth2ServerConfig extends WebSecurityConfigurerAdapter {
             http
                     .antMatcher("/**").authorizeRequests()
                     .antMatchers(HttpMethod.GET, "/is-betting-allowed", "/betting-times").permitAll()
+                    .antMatchers(HttpMethod.GET, "/banner", "/me").hasAnyRole(USER_ROLE, ADMIN_ROLE)
+                    .antMatchers(HttpMethod.POST, "/betting-times").hasRole(ADMIN_ROLE)
+                    .antMatchers(HttpMethod.PUT, "/betting-times").hasRole(ADMIN_ROLE)
                     .anyRequest().authenticated();
         }
     }
